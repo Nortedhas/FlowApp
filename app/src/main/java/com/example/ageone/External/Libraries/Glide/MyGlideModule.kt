fffdb1b18596344f59870a1f800ea3338d9d8e84
Rinefica <rinefica@gmail.com>
2019-08-08 13:26:19 +0300

    Add color shadow for image

--- app/src/main/java/com/example/ageone/External/Libraries/Glide/MyGlideModule.kt
+++ app/src/main/java/com/example/ageone/External/Libraries/Glide/MyGlideModule.kt
@@ -2,6 +2,7 @@ package com.example.ageone.External.Libraries.Glide
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.graphics.drawable.LayerDrawable
 import android.widget.ImageView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
@@ -9,8 +10,11 @@ import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.target.ViewTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.ageone.Application.R
 import com.example.ageone.Application.currentActivity
+import io.github.armcha.coloredshadow.ShadowImageView
 import yummypets.com.stevia.dp
 
 @GlideModule
@@ -34,9 +38,51 @@ fun addImageFromGlide(image: ImageView, uri: String) {
     GlideApp
         .with(image)
         .load(uri)
-        .onlyRetrieveFromCache(true) //only from cache
         .transform(CenterCrop(), RoundedCorners(25F.dp.toInt()))
         .placeholder(placeholder)
         .into(image)
+
+}
+
+fun addImageFromGlideWithShadow(image: ShadowImageView, uri: String) {
+    val circularProgressDrawable = CircularProgressDrawable(currentActivity as Context)
+    circularProgressDrawable.strokeWidth = 15f.dp
+    circularProgressDrawable.centerRadius = 100f.dp
+    circularProgressDrawable.start()
+
+    val placeholderImage = (currentActivity as Activity).resources.getDrawable(R.drawable.kitty)
+
+    val placeholder = LayerDrawable(
+        arrayOf(
+            placeholderImage,
+            circularProgressDrawable
+        )
+    )
+
+    GlideApp
+        .with(image)
+        .load(uri)
+        .transform(CenterCrop(), RoundedCorners(25F.dp.toInt()))
+        .placeholder(placeholder)
+        .into(object : ViewTarget<ImageView, Drawable>(image) {
+            override fun onLoadStarted(placeholder: Drawable?) {
+                super.onLoadStarted(placeholder)
+                image.setImageDrawable(placeholder, withShadow = false)
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+                super.onLoadCleared(placeholder)
+                image.setImageDrawable(placeholder, withShadow = false)
+            }
+
+            override fun onLoadFailed(errorDrawable: Drawable?) {
+                super.onLoadFailed(errorDrawable)
+                image.setImageDrawable(errorDrawable, withShadow = false)
+            }
+
+            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                image.setImageDrawable(resource)
+            }
+        })
 
 }
--- app/src/main/java/com/example/ageone/Modules/Accaunt/AccauntView.kt
+++ app/src/main/java/com/example/ageone/Modules/Accaunt/AccauntView.kt
@@ -13,10 +13,20 @@ import com.example.ageone.External.Libraries.Glide.addImageFromGlide
 import com.google.android.material.textfield.TextInputLayout
 import yummypets.com.stevia.*
 import android.R
+import android.content.Context
 import android.graphics.Outline
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.view.ViewOutlineProvider
-
+import android.widget.ImageView
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.target.ViewTarget
+import com.bumptech.glide.request.transition.Transition
+import com.example.ageone.Application.currentActivity
+import com.example.ageone.External.Libraries.Glide.GlideApp
+import com.example.ageone.External.Libraries.Glide.addImageFromGlideWithShadow
+import io.github.armcha.coloredshadow.ShadowImageView
 
 
 class AccauntView: BaseModule() {
@@ -26,7 +36,7 @@ class AccauntView: BaseModule() {
 
         val btn = BaseButton()
         val textView = BaseTextView()
-        val image = BaseImageView()
+        val image = ShadowImageView(currentActivity as Context)
 
         val textInputL by lazy {
             val textInputL = BaseTextInputLayout()
@@ -76,26 +86,12 @@ class AccauntView: BaseModule() {
             .width(40F.dp)
             .setBackgroundColor(Color.TRANSPARENT)
 
-        addImageFromGlide(image, "https://i.pinimg.com/originals/8c/d2/f5/8cd2f5f7c3b02db7bf60b5ec68d11398.jpg")
-
+        addImageFromGlideWithShadow(image, "https://i.pinimg.com/originals/8c/d2/f5/8cd2f5f7c3b02db7bf60b5ec68d11398.jpg")
 
         btn.constrainTopToBottomOf(toolBar, 8)
         textView.constrainTopToBottomOf(btn, 8)
         image
             .constrainTopToBottomOf(textView, 8)
-
-        val viewOutlineProvider = object : ViewOutlineProvider() {
-            override fun getOutline(view: View, outline: Outline) {
-                // Or read size directly from the view's width/height
-                val size = 40F.dp.toInt()
-                outline.setRect(size, size, size, size)
-            }
-        }
-
-        image.apply {
-            outlineProvider = viewOutlineProvider
-            elevation = 8F.dp
-        }
 
         textInputL
             .fillHorizontally()
