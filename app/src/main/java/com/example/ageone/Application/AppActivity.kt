d0951fe89f28c148d9661a903e9aa72d795ebf6c
Rinefica <rinefica@gmail.com>
2019-08-08 19:40:41 +0300

    add moshi

--- app/src/main/java/com/example/ageone/Application/AppActivity.kt
+++ app/src/main/java/com/example/ageone/Application/AppActivity.kt
@@ -3,11 +3,14 @@ package com.example.ageone.Application
 import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.ageone.External.Base.Activity.BaseActivity
 import com.example.ageone.External.Libraries.Glide.GlideApp
 import com.example.ageone.Network.HTTP.Methods
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import com.swarmnyc.promisekt.Promise
 import timber.log.Timber
 import yummypets.com.stevia.subviews
@@ -23,11 +26,6 @@ class AppActivity: BaseActivity()  {
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 
-        /*//download cache
-        GlideApp
-            .with( this )
-            .download("https://i.pinimg.com/originals/8c/d2/f5/8cd2f5f7c3b02db7bf60b5ec68d11398.jpg")
-            .diskCacheStrategy(DiskCacheStrategy.ALL)*/
 
         coordinator.setLaunchScreen()
         Promise<Unit> { resolve, _ ->
